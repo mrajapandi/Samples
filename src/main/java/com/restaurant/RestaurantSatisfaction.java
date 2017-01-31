af84087b018026be36d5089008f54734b1ebdf63
Rajapandi <rajapandi@techmango.net>
2017-01-31 13:19:53 +0530

    change logger to sysout

--- src/main/java/com/restaurant/RestaurantSatisfaction.java
+++ src/main/java/com/restaurant/RestaurantSatisfaction.java
@@ -3,8 +3,6 @@ package com.restaurant;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-import org.apache.log4j.Logger;
-
 /**This class used to read the file and calculate the maximum satisfaction for the given menu items
  * 
  * @author rajapandi
@@ -12,43 +10,42 @@ import org.apache.log4j.Logger;
  */
 public class RestaurantSatisfaction {
 	
-	public Logger logger = Logger.getLogger(RestaurantSatisfaction.class);
 	
 	public void readFileAndCalculateSatisfaction(){
 	    BufferedReader br=null;
 		try{
 		    br = new BufferedReader(new FileReader("src/main/resources/restaurant_menu_time.txt"));
 			 String text = br.readLine();
-			 logger.info(text);
-			 logger.info("========================");
+			 System.out.println(text);
+			 System.out.println("========================");
 			 String[] timeAndmenuItems = text.split(" ");
-			 Integer totalAmountOfTime = Integer.valueOf(timeAndmenuItems[0]);
-		     Integer totalMenuItems= Integer.valueOf(timeAndmenuItems[1]); 
+			 Integer totalAmountOfTime = Integer.valueOf(timeAndmenuItems[1]);
+		     Integer totalMenuItems= Integer.valueOf(timeAndmenuItems[0]); 
 		     Integer avarageTimeForAllDishes = totalAmountOfTime/totalMenuItems;
 		     Integer dishSatisfaction = null;
 		     Integer timeToEatForDish= null; 
 		     Integer maximumSatisfaction = 0;
-			logger.info("totalAmountOfTime["+totalAmountOfTime+"]totalMenuItems["+totalMenuItems+"]");
+			System.out.println("totalAmountOfTime["+totalAmountOfTime+"]totalMenuItems["+totalMenuItems+"]");
 			   int i=1;
 			    for(String line; (line = br.readLine()) != null; ) {
 			        // process the line.
-			    	logger.info(line);
+			    	System.out.println(line);
 			    	String[] dishAndTimeTaken = line.split(" ");
-			    	dishSatisfaction = Integer.valueOf(dishAndTimeTaken[0]);
-			    	timeToEatForDish = Integer.valueOf(dishAndTimeTaken[1]);
-			    	logger.info("dishSatisfaction["+dishSatisfaction+"] -- timeToEatForDish"+i++ +"["+timeToEatForDish+"]");
+			    	dishSatisfaction = Integer.valueOf(dishAndTimeTaken[1]);
+			    	timeToEatForDish = Integer.valueOf(dishAndTimeTaken[0]);
+			    	System.out.println("dishSatisfaction["+dishSatisfaction+"] -- timeToEatForDish"+i++ +"["+timeToEatForDish+"]");
 			    	if(timeToEatForDish <= avarageTimeForAllDishes){
 			    	   maximumSatisfaction += dishSatisfaction;
 			    	}else{
 			    		maximumSatisfaction += dishSatisfaction;
 			    	}
 			    }
-			    logger.info("maximumSatisfaction["+maximumSatisfaction+"]");
+			    System.out.println("maximumSatisfaction["+maximumSatisfaction+"]");
 			    br.close();
 			}
 		
 		catch(Exception exception){
-			logger.error(exception.getMessage());
+		    System.out.println(exception.getMessage());
 		}
 		
 	}
